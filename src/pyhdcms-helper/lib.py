560b8dafbbef5bf3d66a38bfa77eecd28608cf49
Jason Eveleth <jj11@eveleth.com>
2022-10-18 13:53:00 -0400

    Delete license, add lib.py stuff, rename package

--- src/pyhdcms-helper/lib.py
+++ src/pyhdcms-helper/lib.py
@@ -19,5 +19,22 @@ def regex2stats1d(regex, dir="."):
     filenames = regex2filenames(regex, dir)
     return hdcms.filenames_to_stats_1d(filenames)
 
+def regex2stats2d(regex, dir="."):
+    filenames = regex2filenames(regex, dir)
+    return hdcms.filenames_to_stats_2d(filenames)
+
 # example: regex2stats1d(r"CM1_2_\d.txt", dir="../data")
 
+def file2filenames(filename):
+    with open(filename) as f:
+        return ",".join(f.readlines())
+
+def file2stats1d(filename):
+    return hdcms.filenames_to_stats_1d(file2filenames(filename))
+
+def file2stats2d(filename):
+    return hdcms.filenames_to_stats_2d(file2filenames(filename))
+
+# example: file2stats2d("./compound1_high_res.txt")
+
+# TODO: numpy arrays to stats
