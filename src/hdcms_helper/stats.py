ad72896a10d62476f471bc7f47582fd2d35dd5b6
Jason Eveleth <jj11@eveleth.com>
2022-10-22 16:35:14 -0400

    Raise error for no regex matches

--- src/hdcms_helper/stats.py
+++ src/hdcms_helper/stats.py
@@ -23,13 +23,16 @@ def regex2filenames(regex, dir="."):
     files = [f for f in os.listdir(dir) if os.path.isfile(os.path.join(dir, f))]
 
     r = re.compile(regex)
-    a = []
+    matches = []
     for f in files:
         match = r.match(f)
         if match:
-            a.append(match.group())
+            matches.append(match.group())
 
-    full_paths = list(map(lambda f: os.path.join(dir, f), a))
+    if len(matches) == 0:
+        raise RuntimeError(f"No matches for {regex} in directory {dir}")
+
+    full_paths = list(map(lambda f: os.path.join(dir, f), matches))
     return ','.join(full_paths)
 
 def regex2stats1d(regex, dir="."):
